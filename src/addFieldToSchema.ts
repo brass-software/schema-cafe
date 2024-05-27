40403f934ceffa0b1906b85c656072065e2a6f96
Mike Rybka <merybka@gmail.com>
2024-05-27 07:49:43 -0400

    Scaffold

---
+++ src/addFieldToSchema.ts
@@ -0,0 +1,3 @@
+export default function addFieldToSchema() {
+    console.log("adding feidl to schema")
+}
---
+++ src/index.ts
@@ -0,0 +1,14 @@
+export {default as sendLoginCode} from './sendLoginCode';
+export {default as login} from './login';
+export {default as logout} from './logout';
+export {default as signOut} from './signOut';
+export {default as get} from './get';
+export {default as createFolder} from './createFolder';
+export {default as createSchema} from './createSchema';
+export {default as addFieldToSchema} from './addFieldToSchema';
+export {default as removeFieldFromSchema} from './removeFieldFromSchema';
+export {default as moveSchema} from './moveSchema';
+export {default as updateSchemaDesc} from './updateSchemaDesc';
+export {default as moveSchemaFieldPos} from './moveSchemaFieldPos';
+export {default as changeSchemaFieldType} from './changeSchemaFieldType';
+export {default as renameSchemaField} from './renameSchemaField';
