1d8a5bbeab40d1a5f1ee0cddda90987410eb23f9
Mike Rybka <merybka@gmail.com>
2024-05-28 09:33:31 -0400

    Update

--- release.sh
+++ release.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+if [ $# -eq 0 ] ; then
+    echo "Please prove a commit message."
+    exit 1
+fi
+
+npm run build && \
+git add --all && \
+git commit -m $1 && \
+git push && \
 npm version patch && \
 git push && \
 npm publish --access public
