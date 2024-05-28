df9f01d88434f96005ff0ead36983adf100f029e
Mike Rybka <merybka@gmail.com>
2024-05-28 09:35:33 -0400

    wip

--- release.sh
+++ release.sh
@@ -1,15 +1,9 @@
 #!/bin/sh
 
-if [ $# -eq 0 ] ; then
-    echo "Please prove a commit message."
-    exit 1
-fi
-
 npm run build && \
 git add --all && \
 git commit -m wip && \
 git push && \
-echo "here" && \
 npm version patch && \
 git push && \
 npm publish --access public
